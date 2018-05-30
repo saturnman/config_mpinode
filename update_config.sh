6c6ca14df3ec0eea4e4ea6ba0bad56209f98f01f
saturnman <saturnman@126.com>
2018-05-31 07:30:18 +0800

    update script

--- update_config.sh
+++ update_config.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-while [ true ]
-do 
-    if test -e "ssh_config";then
-        cp ssh_config ~/.ssh/config
-    fi
-    echo 'Run update ssh config script'
-done
+if test -e "ssh_config";then
+    cp ssh_config ~/.ssh/config
+fi
+echo 'Run update ssh config script'
