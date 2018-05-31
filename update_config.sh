417395079800a59cc8f5308a878555bb7cde665c
saturnman <saturnman@126.com>
2018-05-31 10:36:21 +0800

    bugfix of test file exist

--- update_config.sh
+++ update_config.sh
@@ -3,7 +3,7 @@ if test -e "ssh_config";then
     cp ssh_config ~/.ssh/config
 fi
 if test -e "sysHostFilePart";then
-    if test -e "~/hosts.bak";then
+    if test -e "/home/tutorial/hosts.bak";then
         cp ~/hosts.bak /etc/hosts
         cat sysHostFilePart >> /etc/hosts
     fi
